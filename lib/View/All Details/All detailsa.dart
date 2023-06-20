9e1d67d1b1356dbb39100ceaa2fe95b7b6f0b4aa
AbithaVishnu <abitha.pandakkal@gmail.com>
2023-06-20 15:42:01 +0530

    all details

--- lib/View/All Details/All detailsa.dart
+++ lib/View/All Details/All detailsa.dart
@@ -176,7 +176,7 @@ class AllDetails extends StatelessWidget {
                                   ),
 
                                   Stack(
-                                    alignment: Alignment.center,
+                                    alignment: Alignment.bottomCenter,
                                     children: [
                                       Image.asset(
                                         "Assets/Images/Rectangle 1@2x.png",
@@ -184,31 +184,29 @@ class AllDetails extends StatelessWidget {
                                         height: 100,
                                         fit: BoxFit.fill,
                                       ),
-                                      Positioned(
-                                        bottom: 0,
-                                        child: Container(
-                                          width: 112,
-                                          height: 29,
-                                          decoration: BoxDecoration(
+                                      Container(
+                                        width: 112,
+                                        height: 29,
+                                        decoration: BoxDecoration(
+                                          color: Color(0xFF989898),
+                                          borderRadius: BorderRadius.circular(6),
+                                          border: Border.all(
                                             color: Color(0xFF989898),
-                                            borderRadius: BorderRadius.circular(6),
-                                            border: Border.all(
-                                              color: Color(0xFF989898),
-                                              width: 1,
-                                            ),
+                                            width: 1,
                                           ),
-                                          child: Center(
-                                            child: Text(
-                                              '1h 30m',
-                                              style: TextStyle(
-                                                color: Colors.black,
-                                              ),
+                                        ),
+                                        child: Center(
+                                          child: Text(
+                                            '1h 30m',
+                                            style: TextStyle(
+                                              color: Colors.black,
                                             ),
                                           ),
                                         ),
                                       ),
                                     ],
                                   ),
+
                                 ],
                               ),
                               SizedBox(
